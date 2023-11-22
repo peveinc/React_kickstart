22202a81002e3b4aa52080f17dea171a9d624926
peveinc <peveinc@gmail.com>
2023-11-22 14:48:16 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>REACT TEST</h1>
+     <p>Programming in React.</p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Duis pellentesque turpis odio, sed placerat nibh dapibus auctor.
+       Sed ullamcorper lectus eget eros interdum commodo. Aliquam orci ante, hendrerit id lacus eu, feugiat condimentum ipsum. Vestibulum tellus mauris, semper at sodales eu, pellentesque et quam. Fusce sagittis et magna et dictum. Aenean vitae consequat elit, sed ullamcorper nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec sit amet turpis a ex iaculis posuere et eu ex. Pellentesque eget metus felis. Nullam ac luctus felis, ut viverra nunc. Pellentesque sollicitudin aliquam leo. Quisque ultricies eget metus et egestas. Fusce at libero eros. Vestibulum tristique mi tristique, efficitur nisi sit amet, posuere nibh. Maecenas vel arcu eget 
+      turpis auctor cursus vitae et turpis. Donec bibendum efficitur aliquam.</p>
+    </div>
+  );
+}
+
+export default App;
